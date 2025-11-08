c7eaa21509155872ca7f207189f09c2cb51407b1
Amit <freak.amit1@gmail.com>
2025-11-08 14:53:54 +0530

    commit[Anshu]

--- BankManagementSystem/Account.cs
+++ BankManagementSystem/Account.cs
@@ -30,20 +30,9 @@ namespace BankManagementSystem
         {
             Console.WriteLine("You have Create Successfully Your Account");
         }
+
+        public abstract void TransferMoney (Account recever, double Amount);
         
-        public virtual void TransferMoney(Account recever , double Amount)
-        {
-            if (recever == null) 
-            {
-                Console.WriteLine("Not avaliable Data");
-            }
-            if(Amount <= 0)
-            {
-                Console.WriteLine("Can't be negative money");
-            }
-            Balance-=Amount;
-            recever.Deposit(Amount);
-        }
 
     }
 }
